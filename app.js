1d4047e4eab69e7fec8812468dd04d8f84804823
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-08-24 14:16:25 -0400

    added ejs files for footer, header to make same across site; set up about me page; moved date over to its own file and refactored date and app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Shop", "Parts", "ToDo"];
-let workItems = ["Dump Trailer Repair"];
+const items = ["Shop", "Parts", "ToDo"];
+const workItems = ["Dump Trailer Repair"];
 
 app.set("view engine", "ejs");
 
@@ -12,21 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -42,46 +35,14 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
-// var currentDay = today.getDay();
-// var day = "";
-
-// switch (currentDay) {
-//   case 0:
-//     day = "Sunday";
-//     break;
-//   case 1:
-//     day = "Monday";
-//     break;
-//   case 2:
-//     day = "Tuesday";
-//     break;
-//   case 3:
-//     day = "Wednesday";
-//     break;
-//   case 4:
-//     day = "Thursday";
-//     break;
-//   case 5:
-//     day = "Friday";
-//     break;
-//   case 6:
-//     day = "Saturday";
-//     break;
-
-//   default:
-//    console.log("Error: Current day is equal to: "+ currentDay);
-// }
-
-// if (currentDay === 6 || currentDay === 0) {
-//   day = "Weekend";
-// } else {
-//   day = "Weekday";
-// }
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
